1db3ca442a8103c37d7e2fc0f96260170315d2d9
CarpinteroCreativo <61610433+CarpinteroCreativo@users.noreply.github.com>
2025-02-15 19:34:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let saludo = "Hola ";
 let adelanto = 470 + 569 + 327;
 let usuario = ["Julissa","Denis","GSR-4"];
 let tSaldo = "Saldo";
-let tareas = 41 + 100 + 100 + 150 + 200;
+let tareas = 41 + 100 + 100 + 150 + 200 + 100;
 let pagoTarea = 100;
 let saldo = adelanto - tareas;
 
